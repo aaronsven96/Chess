f3d215eda693280b57fa9e8e2fb0b77614059cfe
Aaron <aaronsven96@ggmail.com>
2018-08-02 12:25:40 -0400

    Button

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,14 +4,33 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+import java.awt.Button;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Stage stage;
 	
 	@Override
 	public void create () {
+		stage = new Stage();
+		Table table = new Table();
+		table.setFillParent(true);
+		stage.addActor(table);
+		com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+		style.font= new BitmapFont();
+		TextButton button = new TextButton("Host",style);
+		TextButton button2 = new TextButton("Client",style);
+		table.add(button);
+		table.row();
+		table.add(button2);
+
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
@@ -21,8 +40,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 2/3, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
 		batch.end();
+		stage.draw();
 	}
 	
 	@Override
