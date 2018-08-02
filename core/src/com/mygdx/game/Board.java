d1f88736a9f1d3270a78046ccc08e97fa9256ef4
JFMF <jhn.tly@gmail.com>
2018-08-02 12:26:42 -0400

    Add files via upload

--- core/src/com/mygdx/game/Board.java
+++ core/src/com/mygdx/game/Board.java
@@ -1,23 +1,35 @@
 package com.mygdx.game;
 
 public class Board {
+
+
+
     protected Piece[][] board;
     protected boolean whiteChecked;
     protected boolean blackChecked;
 
     public Board(){
-        board = new Piece[8][8];
         whiteChecked = false;
         blackChecked = false;
 
+        board = new Piece[8][8];
+        for (Piece[] row: board){
+
+        }
     }
 
     protected Board movePiece(String move){
+
         return this;
     }
-    
+
 
     public boolean is() {
         return blackChecked;
+    }
+
+
+    public boolean checkCheckmate(){
+        return false;
     }
 }
--- core/src/com/mygdx/game/Piece.java
+++ core/src/com/mygdx/game/Piece.java
@@ -2,5 +2,14 @@ package com.mygdx.game;
 
 class Piece {
     protected char type;
+    protected boolean white;
+    protected String pos;
+
+    public Piece(char type, String pos, boolean white){
+        this.type = type;
+        this.pos = pos;
+        this.white = white;
+
+    }
 
 }
