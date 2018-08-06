5036786b848886983893de1bcca96da414dbc28b
Aaron <aaronsven96@ggmail.com>
2018-08-06 10:43:46 -0400

    Server

--- core/src/Server/Client.java
+++ core/src/Server/Client.java
@@ -13,15 +13,20 @@ import com.badlogic.gdx.net.SocketHints;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.Pool.Poolable;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import static com.badlogic.gdx.Net.Protocol.TCP;
 
 
 public class Client {
-    public void ConnectToSocket() {
+    ConcurrentLinkedQueue<String> data;
+    public void ConnectToSocket(final ConcurrentLinkedQueue<String> data) {
+        this.data=data;
         new Runnable() {
             @Override
             public void run() {
@@ -34,12 +39,24 @@ public class Client {
                 }
                 InputStream stream =socket.getInputStream();
                 while(true){
-
-
+                    try {
+                        data.add(readBytes(stream));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         };
 
     }
-
+    private String  readBytes(InputStream stream) throws IOException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = stream.read(buffer)) != -1) {
+            result.write(buffer, 0, length);
+        }
+// StandardCharsets.UTF_8.name() > JDK 7
+        return result.toString("UTF-8");
+    }
 }
