e534b201b1bd315370fb41405f76b816c58c90cc
JFMF <28885446+f14t13y@users.noreply.github.com>
2018-08-07 14:32:43 -0400

    allow movement of pawns

--- core/src/com/mygdx/game/Board.java
+++ core/src/com/mygdx/game/Board.java
@@ -19,20 +19,20 @@ public class Board {
         board = new Piece[8][8];
 
 
-        String white = "RNBQKBNRPPPPPPPP";
-        String black = "PPPPPPPPRNBKQBNR";
+        String whiteP = "RNBQKBNRPPPPPPPP";
+        String blackP = "PPPPPPPPRNBKQBNR";
 
 
         // Board setup details
         for (int x = 0; x < 8 ; x++){
             if (x == 0 || x == 1) {
                 for (int y = 0; y < 8; y++) {
-                    board[x][y] = new Piece(black.charAt((y+(x*8)-1)), false,  x, y);
+                    board[x][y] = new Piece(blackP.charAt((y+(x*8)-1)), false,  x, y);
                 }
             }
             else if (x == 6 || x == 7){
                 for (int y = 0; y < 8; y++){
-                    board[x][y] = new Piece(white.charAt((y+(x*8)-1)), true,  x, y);
+                    board[x][y] = new Piece(whiteP.charAt((y+(x*8)-1)), true,  x, y);
                 }
             }
             else{
@@ -49,18 +49,20 @@ public class Board {
         if (this.board[x][y].type != 'B') {
             return this.board[x][y].white != white;
         }
-        return true;
+        return false;
     }
 
-    // fill this out
-    protected Board receiveMove(Piece p, int x2, int y2){
+    // Given a piece and a destination, moves the piece to the cell if valid move
+    protected Board movePiece(Piece p, int x2, int y2){
         if (whiteTurn && !whiteChecked && p.white){         // check if white is eligible to move
             if(p.x - x2 == p.y - y2){
                 if ((p.x - x2 > 1) && (p.type != 'P')){      // make sure not pawn
 
                 }
-                else if (p.x - x2 == 1){
-
+                else if (p.x - x2 == 1 && ((p.type == ('Q') || p.type =='B' || p.type == 'P'))){
+                    if (p.type == 'P' && isEnemyInCell(x2, y2, p.white)){   // if pawn and able to
+                        board[x2][y2] = p;                                  // capture, moves piece
+                    }
                 }
                 else{
 
