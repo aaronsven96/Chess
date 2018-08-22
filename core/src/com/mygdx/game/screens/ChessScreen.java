b079e30bc33a860adf875eddfdfbed30bb1531e6
Aaron <aaronsven96@ggmail.com>
2018-08-22 12:20:07 -0400

    Server Update

---
+++ core/src/com/mygdx/game/screens/ChessScreen.java
@@ -0,0 +1,49 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.mygdx.game.MyGdxGame;
+
+public class ChessScreen implements Screen {
+    Label label;
+    Stage stage;
+    private MyGdxGame game;
+    public ChessScreen(MyGdxGame game, Thread tcpSys){
+        this.game=game;
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
