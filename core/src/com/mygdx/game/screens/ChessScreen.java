461d29188d12c417ed2dfe4b38a7f10b422b9513
Aaron <aaronsven96@ggmail.com>
2018-08-28 11:51:46 -0400

    Chess Game

--- core/src/com/mygdx/game/screens/ChessScreen.java
+++ core/src/com/mygdx/game/screens/ChessScreen.java
@@ -12,11 +12,8 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.Board;
-import com.mygdx.game.Cell;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.Piece;
-
-import java.util.ArrayList;
 
 
 public class ChessScreen implements Screen, InputProcessor {
@@ -29,6 +26,13 @@ public class ChessScreen implements Screen, InputProcessor {
     private MyGdxGame game;
     private static final int spaceSize=50;
     private Board chessGame;
+    private Boolean whiteTurn;
+    private Boolean movPiece=false;
+    private int startX =0;
+    private int startY =0;
+    private int movX;
+    private int movY;
+
     public ChessScreen(MyGdxGame game, Thread tcpSys){
         this.game=game;
     }
@@ -37,12 +41,16 @@ public class ChessScreen implements Screen, InputProcessor {
     }
     @Override
     public void show() {
+        whiteTurn=true;
         chessGame=new Board();
-        Gdx.input.setInputProcessor(this.stage);
+        Gdx.input.setInputProcessor(this);
+
         TextureAtlas bAtlas=new TextureAtlas(Gdx.files.internal("chess/black_pieces/BlackPieces.atlas"));
         TextureAtlas wAtlas=new TextureAtlas(Gdx.files.internal("chess/white_pieces/WhitePieces.atlas"));
+
         bPieces=bAtlas.createSprites();
         wPieces=wAtlas.createSprites();
+
         board=new Texture(Gdx.files.internal("chess/board.png"));
         OrthographicCamera camera = new OrthographicCamera();
         camera.setToOrtho(false,400,400);
@@ -58,10 +66,15 @@ public class ChessScreen implements Screen, InputProcessor {
     private void drawPieces(Board game,SpriteBatch batch){
         for (int x=0;x<8;x++){
             for(int y=0;y<8;y++){
-                Piece piece=game.getCell(x,y).getPiece();
-                Sprite sPiece=getCharToSprite(piece);
-                if (sPiece!=null) {
-                    drawAtPosition(sPiece, batch, x, y);
+                Piece piece = game.getCell(x, y).getPiece();
+                Sprite sPiece = getCharToSprite(piece);
+                if (sPiece != null) {
+                    if (startX ==y && startY ==7-x&&movPiece) {
+                        batch.draw(sPiece,movX-(sPiece.getWidth()/2),400-movY-(sPiece.getHeight()/2));
+                    }
+                    else{
+                        drawAtPosition(sPiece, batch, x, y);
+                    }
                 }
             }
         }
@@ -89,7 +102,13 @@ public class ChessScreen implements Screen, InputProcessor {
     }
 
     public void drawAtPosition(Sprite piece, SpriteBatch batch,int x,int y){
-        batch.draw(piece,x*spaceSize+(spaceSize/2)-(piece.getWidth()/2),y*spaceSize);
+        int posY=x;
+        int posX=y;
+        if(!whiteTurn){
+            posX=7-y;
+            posY=7-x;
+        }
+        batch.draw(piece,posX*spaceSize+(spaceSize/2)-(piece.getWidth()/2),posY*spaceSize);
     }
     @Override
     public void resize(int width, int height) {
@@ -132,17 +151,30 @@ public class ChessScreen implements Screen, InputProcessor {
     }
 
     @Override public boolean touchDown (int screenX, int screenY, int pointer, int button) {
+        movPiece=true;
+        movY=screenY;
+        movX=screenX;
+        startX = screenX/50;
+        startY = screenY/50;
+        System.out.println("x:"+ startX +" y:"+ startY);
+
         // ignore if its not left mouse button or first touch pointer
         return true;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        System.out.println("moving from"+startY+":"+(7-startX)+"to"+(screenY/50)+":"+(7-screenX/50));
+        chessGame.movePiece(7-startY,startX,7-screenY/50,(screenX/50));
+        movPiece=false;
         return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
+
+        movX=screenX;
+        movY=screenY;
         return false;
     }
 
